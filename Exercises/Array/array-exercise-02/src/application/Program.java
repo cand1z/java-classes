dd7ec49375bb84932488265ff8fadd5d32debb04
cand1z <daviaraujocandido@gmail.com>
2023-04-25 20:50:11 -0300

    update(array-exercise-02): final

--- Exercises/Array/array-exercise-02/src/application/Program.java
+++ Exercises/Array/array-exercise-02/src/application/Program.java
@@ -1,7 +1,37 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("How many numbers you will put? ");
+        int n = scanner.nextInt();
+        double [] vect = new double[n];
+        double sum = 0;
+
+        for (int i = 0; i < vect.length; i++) {
+            scanner.nextLine();
+            System.out.print("Digit a number: ");
+            vect[i] = scanner.nextDouble();
+            sum += vect[i];
+        }
+
+        System.out.print("VALUES = ");
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print(vect[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        double avg = sum / vect.length;
+        System.out.println("SUM = " + sum);
+        System.out.println("AVERAGE = "+ avg);
+
+        scanner.close();
     }
 }
