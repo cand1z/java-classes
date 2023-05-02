bfc3fc29eb315f3af165b876b3ffbe51765786c8
cand1z <daviaraujocandido@gmail.com>
2023-05-02 01:51:58 -0300

    update(matrix2): final version exercise

--- Exercises/Matrix/matrix2/src/application/Program.java
+++ Exercises/Matrix/matrix2/src/application/Program.java
@@ -1,4 +1,48 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of lines: ");
+        int m = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int n = scanner.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        int number = scanner.nextInt();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == number) {
+                    System.out.println("Position " + i + ", " + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + matrix[i][j - 1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + matrix[i - 1][j]);
+                    }
+                    if (j < matrix[i].length) {
+                        System.out.println("Rigth: " + matrix[i][j + 1]);
+                    }
+                    if (i < matrix.length){
+                        System.out.println("Down: " + matrix[i + 1][j]);
+                    }
+                }
+            }
+        }
+
+        scanner.close();
+    }
 }
