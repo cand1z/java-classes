6bd579fd0328c316b70e7dd7e6807634b211acf6
cand1z <daviaraujocandido@gmail.com>
2023-05-07 22:05:16 -0300

    add(enum-2): exercise about enums

---
+++ Exercises/Enum/enum-2/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import entities.Comment;
+import entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comment c1 = new Comment("Have a nice trip");
+        Comment c2 = new Comment("Wow that's awesome!");
+        Post post1 = new Post(
+                sdf.parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful contry!",
+                12);
+        post1.addComment(c1);
+        post1.addComment(c2);
+
+        Comment c3 = new Comment("Good night");
+        Comment c4 = new Comment("May the Force be with you");
+        Post post2 = new Post(
+                sdf.parse("28/07/2018 14:18:22"),
+                "Good night guys",
+                "See you tomorrow",
+                5);
+        post2.addComment(c3);
+        post2.addComment(c4);
+
+        System.out.println(post1);
+        System.out.println(post2);
+
+    }
+}
