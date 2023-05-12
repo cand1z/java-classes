d9c1c1dbb0c998b8eb98f2f57503e037791e2bcf
cand1z <daviaraujocandido@gmail.com>
2023-05-12 00:01:38 -0300

    learning about upcast and downcast

--- Classes/inheritance-polymorphism/src/application/Program.java
+++ Classes/inheritance-polymorphism/src/application/Program.java
@@ -1,11 +1,37 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
 
         BusinessAccount account = new BusinessAccount();
 
+        Account acc = new Account(1001, "Davi", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Alex", 2.0, 500.0);
+
+        // UPCAST
+        Account acc1 = bacc;
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new BusinessAccount(1003, "Maria", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 13.50);
+
+        // DOWNCAST
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(400);
+
+        if (acc2 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount) acc2;
+            acc5.loan(500);
+            System.out.println("Loan!");
+        }
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
     }
 }
