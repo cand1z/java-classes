fddf8e31a80cabd5d4b52b9868e70f7289c60a66
cand1z <daviaraujocandido@gmail.com>
2023-05-14 19:38:03 -0300

    learning about final methods and classes

---
+++ Classes/inheritance-polymorphism/src/entities/SavingsAccountPlus.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class SavingsAccountPlus extends SavingsAccount{
+
+    // Show error
+    /*
+    @Override
+    public void withdraw(double amount){
+        balance -= amount + 2;
+    }
+     */
+}
