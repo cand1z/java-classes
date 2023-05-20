af9045940ed6e7e27b83921d8088073df009c630
cand1z <daviaraujocandido@gmail.com>
2023-05-20 03:00:55 -0300

    learning about try catch

--- Classes/try-catch/src/application/Program.java
+++ Classes/try-catch/src/application/Program.java
@@ -1,11 +1,26 @@
 package application;
 
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        method1();
+
+        System.out.println("End of program");
+    }
+
+    public static void method1() {
+        System.out.println("*** STARTING METHOD 1 ***");
+        method2();
+        System.out.println("*** END OF METHOD 1 ***");
+    }
+
+    public static void method2(){
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("*** STARTING METHOD 2 ***");
 
         try {
             System.out.print("Enter names: ");
@@ -15,12 +30,13 @@ public class Program {
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
+            e.printStackTrace();
+            scanner.next();
         } catch (InputMismatchException e) {
             System.out.println("Input error!");
         }
-
-        System.out.println("End of program");
-
         scanner.close();
+
+        System.out.println("*** END OF METHOD 2 ***");
     }
 }
